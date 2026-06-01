f6eceabeb1c58fe7740fc448f7b4d2c9aa60bfad
lavhalerashmi-oss <lavhalerashmi@gmail.com>
2026-06-01 13:14:31 +0530

    sum_even_number.py

---
+++ sum in loops/sum_even_number.py
@@ -0,0 +1,7 @@
+#print the sum of even number as per range
+n= int(input("Enter a number: "))
+sum=0
+for i in range(2,n+1,2):
+    sum = sum +i
+print("This is sum of n even number", sum)
+#  it print the sum of even number till n.
